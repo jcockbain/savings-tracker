3766965f9229c6855bbd1d2297fbe3c3c2637c39
James Cockbain <james.cockbain@ibm.com>
2020-02-19 19:37:21 +0000

    updated crud operations

--- main.go
+++ main.go
@@ -5,35 +5,73 @@ import (
 	"fmt"
 	"log"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-// Trainer : a trainer
-type Trainer struct {
+// Person : a trainer
+type Person struct {
 	Name string
 	Age  int
 	City string
 }
 
 func main() {
-	// Rest of the code will go here
-	// Set client options
 	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
-
-	// Connect to MongoDB
 	client, err := mongo.Connect(context.TODO(), clientOptions)
-
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// Check the connection
 	err = client.Ping(context.TODO(), nil)
-
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	fmt.Println("Connected to MongoDB!")
+
+	collection := client.Database("mydb").Collection("persons")
+
+	james := Person{"James", 23, "Winchester"}
+	john := Person{"John", 32, "Nairobi"}
+	frankie := Person{"Frankie", 31, "Nairobi"}
+	people := []interface{}{john, frankie}
+	var result Person
+
+	insertResult, err := collection.InsertOne(context.TODO(), james)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Inserted a Single Document: ", insertResult.InsertedID)
+
+
+	insertManyResult, err := collection.InsertMany(context.TODO(), people)
+    if err != nil {
+      log.Fatal(err)
+  	}
+	fmt.Println("Inserted multiple documents: ", insertManyResult.InsertedIDs)
+
+	filter := bson.D{}
+    update := bson.D{
+        {"$inc", bson.D{
+            {"age", 1},
+        }},
+    }
+
+    updateResult, err := collection.UpdateOne(context.TODO(), filter, update)
+    if err != nil {
+      log.Fatal(err)
+  	}
+	fmt.Printf("Matched %v documents and updated %v documents.\n", updateResult.MatchedCount, updateResult.ModifiedCount)
+
+
+	err = collection.FindOne(context.TODO(), filter).Decode(&result)
+  	if err != nil {
+  	  log.Fatal(err)
+  	}
+  	fmt.Printf("Found a single document: %+v\n", result)
+
+  	findOptions := options.Find()
+    findOptions.SetLimit(2)
 }
