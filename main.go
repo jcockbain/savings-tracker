555343f1783475080f803bdca3b50b2c3019c98e
James Cockbain <james.cockbain@ibm.com>
2020-02-19 19:44:14 +0000

    add delete

--- main.go
+++ main.go
@@ -45,33 +45,66 @@ func main() {
 	}
 	fmt.Println("Inserted a Single Document: ", insertResult.InsertedID)
 
-
 	insertManyResult, err := collection.InsertMany(context.TODO(), people)
-    if err != nil {
-      log.Fatal(err)
-  	}
+	if err != nil {
+		log.Fatal(err)
+	}
 	fmt.Println("Inserted multiple documents: ", insertManyResult.InsertedIDs)
 
 	filter := bson.D{}
-    update := bson.D{
-        {"$inc", bson.D{
-            {"age", 1},
-        }},
-    }
+	update := bson.D{
+		{"$inc", bson.D{
+			{"age", 1},
+		}},
+	}
 
-    updateResult, err := collection.UpdateOne(context.TODO(), filter, update)
-    if err != nil {
-      log.Fatal(err)
-  	}
+	updateResult, err := collection.UpdateOne(context.TODO(), filter, update)
+	if err != nil {
+		log.Fatal(err)
+	}
 	fmt.Printf("Matched %v documents and updated %v documents.\n", updateResult.MatchedCount, updateResult.ModifiedCount)
 
-
 	err = collection.FindOne(context.TODO(), filter).Decode(&result)
-  	if err != nil {
-  	  log.Fatal(err)
-  	}
-  	fmt.Printf("Found a single document: %+v\n", result)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Found a single document: %+v\n", result)
 
-  	findOptions := options.Find()
-    findOptions.SetLimit(2)
+	findOptions := options.Find()
+	findOptions.SetLimit(2)
+
+	var results []*Person
+	cur, err := collection.Find(context.TODO(), bson.D{}, findOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for cur.Next(context.TODO()) {
+		var elem Person
+		err := cur.Decode(&elem)
+		if err != nil {
+			log.Fatal(err)
+		}
+		results = append(results, &elem)
+	}
+
+	if err := cur.Err(); err != nil {
+		log.Fatal(err)
+	}
+	cur.Close(context.TODO())
+	fmt.Printf("Found multiple documents (array of pointers): %+v\n", results)
+
+	deleteResult, err := collection.DeleteMany(context.TODO(), bson.D{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Deleted %v documents in the persons collection\n", deleteResult.DeletedCount)
+
+	err = client.Disconnect(context.TODO())
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		fmt.Println("Connection to MongoDB closed.")
+	}
 }
